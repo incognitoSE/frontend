edffd395969b2bcf6f356b213ae5cc5e111489e5
Niloufar Alikhani <niloufaraliikhani79@gmail.com>
2021-05-17 13:48:03 +0430

    change in jwt and serviceshows

--- src/components/ServiceShows.vue
+++ src/components/ServiceShows.vue
@@ -3,7 +3,7 @@
     <v-row v-for="(item, index) in items" :key="index" dense no-gutters>
       <v-col cols="12">
         <v-card :color="item.color" tile>
-          <div v-if="item.id !== 2">
+          <div v-if="item.id !== 2 && item.id !== 3">
             <div class="d-flex align-center">
               <v-spacer></v-spacer>
               <v-col cols="6">
@@ -26,15 +26,17 @@
                 <div>
                   <v-card-actions>
                     <v-btn
-                    
-                      class="white--text mt-8 mr-10"
+                      class=" mt-8 mr-10"
                       block
+                      style="color: rgba(255, 255, 255, 1)"
                       large
                       elevation="17"
                       outlined
                       :to="{ name: 'servicehouse' }"
                     >
-                      <h3>استفاده از سرویس</h3>
+                      <h3 style="color: rgba(255, 255, 255, 1)">
+                        استفاده از سرویس
+                      </h3>
                     </v-btn>
                   </v-card-actions>
                 </div>
@@ -46,7 +48,52 @@
               </v-col>
             </div>
           </div>
-          <div v-else class="d-flex">
+          <div v-if="item.id !== 2 && item.id !== 1">
+            <div class="d-flex align-center">
+              <v-spacer></v-spacer>
+              <v-col cols="6">
+                <div>
+                  <v-card-title>
+                    <v-spacer></v-spacer>
+                    <h1
+                      v-html="item.title"
+                      style="color: rgba(255, 255, 255, 1)"
+                    ></h1>
+                    <v-spacer></v-spacer>
+                  </v-card-title>
+                  <div>
+                    <h3
+                      style="color: rgba(255, 255, 255, 1)"
+                      v-html="item.text"
+                    ></h3>
+                  </div>
+                </div>
+                <div>
+                  <v-card-actions>
+                    <v-btn
+                      class=" mt-8 mr-10"
+                      style="color: rgba(255, 255, 255, 1)"
+                      block
+                      large
+                      outlined
+                      elevation="17"
+                      :to="{ name: 'ServiceSim' }"
+                    >
+                      <h3 style="color: rgba(255, 255, 255, 1)">
+                        استفاده از سرویس
+                      </h3>
+                    </v-btn>
+                  </v-card-actions>
+                </div>
+              </v-col>
+              <v-col cols="5">
+                <v-avatar class="modal-dialog  ml-6 my-3" size="cover" tile>
+                  <v-img :src="item.src"> </v-img>
+                </v-avatar>
+              </v-col>
+            </div>
+          </div>
+          <div v-if="item.id != 1 && item.id != 3" class="d-flex">
             <v-col cols="5">
               <v-avatar class="modal-dialog-car my-3 mr-6" size="cover" tile>
                 <v-img :src="item.src"></v-img>
@@ -74,7 +121,7 @@
                   large
                   outlined
                   elevation="17"
-                 :to="{ name: 'ServiceCar' }"
+                  :to="{ name: 'ServiceCar' }"
                 >
                   <h3>استفاده از سرویس</h3>
                 </v-btn>
@@ -98,7 +145,6 @@ export default {
           src: require("../assets/houseicon.png"),
           text:
             " سرویس تخمین قیمت خانه به این صورت است که ویژگی های مورد نظر کاربر را دریافت کرده وقیمت حدودی این خانه را به کاربر می گوید علاوه براین که قیمت خانه های مشابه هم نمایش میدهد."
-        
         },
         {
           id: 2,
@@ -145,9 +191,9 @@ div {
   font-family: IRANSans !important;
   text-align: right;
 }
-h3 {
-  font-family: IRANSans !important;
+h3{
   text-align: right;
+  font-family: IRANSans !important;
 }
 .myflex {
   float: right;
